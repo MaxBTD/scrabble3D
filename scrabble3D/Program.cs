53be3e643b032c46691550c82bb9872fbe86b3e1
MaxBTD <97893081+MaxBTD@users.noreply.github.com>
2024-12-20 14:12:18 +0100

    Dodaj pliki projektów.

---
+++ scrabble3D/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace scrabble3D
+{
+    internal class Program
+    {
+        static string porowLiter(char[] haslo, List<char> literyZKostek)
+        {
+            foreach (char c in haslo) {
+                if (!literyZKostek.Contains(c))
+                    return "NIE";
+                literyZKostek.Remove(c);
+            }
+
+            return "TAK";
+        }
+
+        static void Main(string[] args)
+        {
+            List<string> wyniki = new List<string>();
+            int.TryParse(Console.ReadLine(), out int n);
+            for(int i = 0; i < n; i++)
+            {
+                int.TryParse(Console.ReadLine(), out int k);
+                char[] haslo = Console.ReadLine().ToCharArray();
+                List<char> literyZKostek = new List<char>();
+                for(int j = 0; j < k; j++)
+                {
+                    string[] kostka = Console.ReadLine().Split(' ');
+                    foreach(string c in kostka) {
+                        if (haslo.Contains(c[0]))
+                        {
+                            literyZKostek.Add(c[0]);
+                            break;
+                        }
+
+                    }
+                }
+                wyniki.Add(porowLiter(haslo, literyZKostek));
+            }
+
+            foreach(string w in wyniki)
+                Console.WriteLine(w);
+        }
+    }
+}
